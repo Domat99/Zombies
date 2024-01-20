81b99dc94f88ede5c847d282f51b35a2ce624e6d
Domat99 <domatalkhoury10@gmail.com>
2024-01-20 17:59:40 -0500

    data

---
+++ Zombies/src/zombies/Zombies.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombies;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author domat
+ */
+public class Zombies extends Application {
+
+    public static Stage stg;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        stg = primaryStage;
+        stg.setResizable(true);
+
+        stg.centerOnScreen();
+
+        Parent root1 = FXMLLoader.load(getClass().getResource("Main_page_GUI.fxml"));
+
+        Scene scene1 = new Scene(root1);
+
+        primaryStage.setTitle("Zombies Game");
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
